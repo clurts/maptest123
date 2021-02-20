b102b86b1b64fa49c0736d8513f722d49011c9a6
clurts <clu@rts.dk>
2021-02-20 17:08:12 +0100

    use react-mapbox

--- src/components/Mapcomponent.js
+++ src/components/Mapcomponent.js
@@ -1,51 +1,37 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react"
 
-import mapboxgl from 'mapbox-gl';
+import ReactMapboxGl from "react-mapbox-gl";
 import { useState, useEffect } from 'react';
 
 const Mapcomponent = () => {
     const [mapLng, setMapLng] = useState(null);
     const [mapLat, setMapLat] = useState(null);
     const [mapZoom, setMapZoom] = useState(null);
+    const styleUrl = `mapbox://styles/clurts/ckchfn96u218w1iphn1m6u1fc`;
 
-    mapboxgl.accessToken = 'pk.eyJ1IjoiY2x1cnRzIiwiYSI6ImNrbGRxZmR4dTExZnYydXM0cms3d2c4d2sifQ.yMw8k9ubWM_YBv0DJw5bqw'
+    const Map = ReactMapboxGl({
+        accessToken: 'pk.eyJ1IjoiY2x1cnRzIiwiYSI6ImNrbGRxZmR4dTExZnYydXM0cms3d2c4d2sifQ.yMw8k9ubWM_YBv0DJw5bqw'
+    });
 
     useEffect(() => {
         setMapLng(12.58655283549853)
         setMapLat(55.70512146030345)
         setMapZoom(15)
     }, []);
-    useEffect(() => {
-        let map = new mapboxgl.Map({
-            container: 'mapContainer',
-            style: 'mapbox://styles/clurts/ckchfn96u218w1iphn1m6u1fc',
-            center : [mapLng, mapLat],
-            zoom: mapZoom
-        })
-
-        let geolocate = new mapboxgl.GeolocateControl({
-            positionOptions: {
-            enableHighAccuracy: true
-            },
-            trackUserLocation: true
-            })
-        
-
-        map.addControl(geolocate)
-        geolocate.on('geolocate', function() {
-            console.log('A geolocate event has occurred.')
-            });
-    }, [mapLat, mapLng, mapZoom]);
+    
     const style = css`
         height: 50vh
     `;
  
         
     return ( 
-        <div className="map" css={style} id="mapContainer">
-
-        </div>
+        <Map 
+        css={style}
+        style={styleUrl}
+        center={[mapLng, mapLat]}
+        zoom={[mapZoom]}
+        />
      );
 }
  
