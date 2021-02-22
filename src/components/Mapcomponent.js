94fb3d4d573adc31c622c4bf5095aaabfce34213
clurts <clu@rts.dk>
2021-02-22 15:17:36 +0100

    change mapbox package

--- src/components/Mapcomponent.js
+++ src/components/Mapcomponent.js
@@ -1,34 +1,36 @@
-import ReactMapboxGl from "react-mapbox-gl";
+import ReactMapGL, { GeolocateControl } from "react-map-gl";
 import { useState, useEffect } from 'react';
 
 const Mapcomponent = () => {
-    const [mapLng, setMapLng] = useState(null);
-    const [mapLat, setMapLat] = useState(null);
-    const [mapZoom, setMapZoom] = useState(null);
-
-    const Map = ReactMapboxGl({
-        accessToken: process.env.REACT_APP_MAPBOX_TOKEN
+   
+    const [viewport, setViewport] = useState({
+        latitude: 55.70512146030345,
+        longitude: 12.58655283549853,
+        zoom: 15,
+        width: "100vw",
+        height: "50vh"
     });
-
-    useEffect(() => {
-        setMapLng(12.58655283549853)
-        setMapLat(55.70512146030345)
-        setMapZoom(15)
-    }, []);
-
-    console.log(mapLng,mapLat,mapZoom)
+    const geolocateControlStyle= {
+        right: 10,
+        top: 10
+      }; 
     
-        
+         
     return ( 
-
-        //eslint-disable-next-line
-        <Map 
-            style="mapbox://styles/clurts/ckchfn96u218w1iphn1m6u1fc"
-            containerStyle={{
-                height: '50vh',
-                width: '100vw'
-            }}
-        />
+        <ReactMapGL 
+            {...viewport}
+            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+            mapStyle="mapbox://styles/clurts/ckchfn96u218w1iphn1m6u1fc"
+            onViewportChange={setViewport}
+        >
+           <GeolocateControl
+        style={geolocateControlStyle}
+        positionOptions={{enableHighAccuracy: true}}
+        trackUserLocation={true}
+        showAccuracyCircle={true}
+        auto
+        /> 
+        </ReactMapGL>
      );
 }
  
